dc5104d2bab2ef2cc0345c386eb0b13fb44d2ffb
terahashi <tkshplant@outlook.jp>
2025-11-16 16:11:01 +0900

    section8修正 GSAPで小アニメーション(画像が上下に揺れる)

--- src/js/gsap_make.js
+++ src/js/gsap_make.js
@@ -529,16 +529,8 @@ if (document.body.classList.contains('page-animation')) {
   });
 }
 
-//////////////////////section8 パララックス//////////////////////
-// 1. 画像のパララックス：上下1~2px揺れ（無限ループ）
-gsap.to('.hero-image img', {
-  y: '+=10', // 下方向に少し動く
-  repeat: -1, // 無限ループ
-  yoyo: true, // 上下に戻る
-  duration: 2,
-  ease: 'sine.inOut',
-});
-// 2. 画像の斜めフェードイン（ページ読み込み時）
+//////////////////////section8 小アニメーション//////////////////////
+// 画像の斜めフェードイン（ページ読み込み時）
 gsap.from('.hero-image img', {
   opacity: 0,
   x: -50,
@@ -546,10 +538,20 @@ gsap.from('.hero-image img', {
   duration: 1.5,
   ease: 'power2.out',
 });
-// 3. サブテキストのフェードイン
+
+// 画像の上下1~2px揺れ（無限ループ）
+gsap.to('.hero-image img', {
+  y: '+=10', // 下方向に少し動く
+  repeat: -1, // 無限ループ
+  yoyo: true, // 上下に戻る
+  duration: 2,
+  ease: 'sine.inOut',
+});
+
+// サブテキストのフェードイン
 gsap.to('.sub-text', {
   opacity: 1,
-  y: -30,
+  y: -30, //下から上にフェードイン
   duration: 1,
   delay: 1, // 画像が動いたあと
   ease: 'power2.out',
