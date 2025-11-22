8dd34484f65e0426505d16e476830a339b35d92c
terahashi <tkshplant@outlook.jp>
2025-11-23 01:44:37 +0900

    hamburger.js完成 明日はGSAPなどの他のメニュー作成

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,1 +1,15 @@
 ////hamburger.jsは「common.js」としてインポートされます
+
+const hamburger = document.getElementById('hamburger-id');
+const nav = document.getElementById('nav-id');
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  hamburger.classList.toggle('active');
+
+  if (nav.classList.contains('active')) {
+    console.log('activeついてるよ');
+  } else {
+    console.log('active外れたよ');
+  }
+});
